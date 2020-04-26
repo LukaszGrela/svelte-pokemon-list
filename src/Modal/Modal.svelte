27970f4cb18566b9a5b0bd9d7d734664aaf02732
Lukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2020-04-26 23:35:55 +0100

    Add Modal component

---
+++ src/Modal/Modal.svelte
@@ -0,0 +1,94 @@
+<script>
+  import { onMount } from 'svelte'
+  import { createEventDispatcher, onDestroy } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+  const close = () => dispatch('close')
+
+  let modal
+
+  const handle_keydown = e => {
+    if (e.key === 'Escape') {
+      close()
+      return
+    }
+
+    if (e.key === 'Tab') {
+      // trap focus
+      const nodes = modal.querySelectorAll('*')
+      const tabbable = Array.from(nodes).filter(n => n.tabIndex >= 0)
+
+      let index = tabbable.indexOf(document.activeElement)
+      if (index === -1 && e.shiftKey) index = 0
+
+      index += tabbable.length + (e.shiftKey ? -1 : 1)
+      index %= tabbable.length
+
+      tabbable[index].focus()
+      e.preventDefault()
+    }
+  }
+
+  const previously_focused =
+    typeof document !== 'undefined' && document.activeElement
+
+  if (previously_focused) {
+    onDestroy(() => {
+      previously_focused.focus()
+    })
+  }
+
+  onMount(() => {
+    const element = document.getElementsByTagName('body')[0]
+    element.classList.remove('has-modal')
+    element.classList.add('has-modal')
+    return () => {
+      document.getElementsByTagName('body')[0].classList.remove('has-modal')
+    }
+  })
+</script>
+
+<style>
+  :global(.has-modal) {
+    overflow: hidden;
+  }
+  .portal {
+    z-index: 1979;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .modal-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.3);
+  }
+
+  .modal {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: calc(100vw - 4em);
+    max-width: 32em;
+    max-height: calc(100vh - 4em);
+    overflow: auto;
+    transform: translate(-50%, -50%);
+    padding: 1em;
+    border-radius: 0.2em;
+    background: white;
+  }
+</style>
+
+<svelte:window on:keydown={handle_keydown} />
+<div class="portal">
+  <div class="modal-background" on:click={close} />
+
+  <div class="modal" role="dialog" aria-modal="true" bind:this={modal}>
+    <slot {close} />
+  </div>
+</div>
